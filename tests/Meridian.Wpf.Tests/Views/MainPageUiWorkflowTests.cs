d833af6ec885a590e1799b646107c819681c9a3c
rodoHasArrived <55965792+rodoHasArrived@users.noreply.github.com>
2026-07-15 23:50:29 -0700

    fix(wpf): fill workflow content region

--- tests/Meridian.Wpf.Tests/Views/MainPageUiWorkflowTests.cs
+++ tests/Meridian.Wpf.Tests/Views/MainPageUiWorkflowTests.cs
@@ -336,11 +336,13 @@ public sealed class MainPageUiWorkflowTests
 
                 facade.OpenCommandPalettePage("Backtest");
                 await WaitForConditionAsync(() => facade.ViewModel.IsWorkflowPageActive).ConfigureAwait(true);
+                await WaitForConditionAsync(() => facade.ContentFrame.ActualWidth > 0).ConfigureAwait(true);
 
                 facade.ViewModel.ShellDensityMode.Should().Be(ShellDensityMode.Standard);
                 facade.ViewModel.ShellContextVisibility.Should().Be(Visibility.Collapsed);
                 facade.WorkspaceShellContextStrip.Visibility.Should().Be(Visibility.Collapsed);
                 facade.ContentFrame.Visibility.Should().Be(Visibility.Visible);
+                facade.ContentFrame.ActualWidth.Should().BeGreaterThan(0);
                 facade.WorkspaceHomeSurface.Visibility.Should().Be(Visibility.Collapsed);
 
                 facade.Click(facade.ShellDensityToggleButton);
